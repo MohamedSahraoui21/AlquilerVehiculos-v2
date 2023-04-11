366df9e01fe4103da310d1727c918cec5c258a75
SIMO <SIMO@DESKTOP-QGHA210>
2023-04-11 15:18:19 +0200

    todos los test bien pero me sale un error al ejecutar

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/Vehiculos.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/ficheros/Vehiculos.java
@@ -25,7 +25,7 @@ import javax.xml.parsers.ParserConfigurationException;
 public class Vehiculos implements IVehiculos {
 	
 	private static final File FICHERO_VEHICULOS = new File(
-			"C:\\Users\\Archen\\git\\AlquilerVehiculos-v2\\datos\\vehiculos.xml");
+			"datos%svehiculos.xml");
 	private static final String RAIZ = "vehiculos";
 	private static final String VEHICULO = "vehiculo";
 	private static final String MARCA = "marca";
